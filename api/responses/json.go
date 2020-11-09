184d342f661c6161ce66f2462c8b6c3aeee4af0e
Ahmad Tri <ahmadtri26@gmail.com>
2020-11-09 09:36:10 +0700

    json return with status code

--- api/responses/json.go
+++ api/responses/json.go
@@ -17,9 +17,11 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 func ERROR(w http.ResponseWriter, statusCode int, err error) {
 	if err != nil {
 		JSON(w, statusCode, struct {
-			Error string `json:"error"`
+			Status int    `json:"status"`
+			Error  string `json:"error"`
 		}{
-			Error: err.Error(),
+			Status: statusCode,
+			Error:  err.Error(),
 		})
 		return
 	}
